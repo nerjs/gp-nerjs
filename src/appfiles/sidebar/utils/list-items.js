f1087e6f5a9e39383b6444aa6ed5ac9a4e7d6d46
karifan <karifan.stap@gmail.com>
2018-05-09 23:23:37 +0300

    update menu rotate

--- src/appfiles/sidebar/utils/list-items.js
+++ src/appfiles/sidebar/utils/list-items.js
@@ -3,10 +3,17 @@ const ListItem = require('./list-item')
 
 const ListItems = ({list, active}) => {
 	return (
-		<ul className="list-items" style={{
-			height: active ? list.length * 20 : 0 
-		}} >
-			{list.map((l,i) => <li key={i} className="list-item"><ListItem item={l} index={i} /></li>)}
+		<ul className="list-items" >
+			{list.map((l,i) => (
+				<li key={i} onClick={()=>alert(123)}
+					className="list-item" 
+					style={{
+						transitionDelay: `${i*0.1}s`
+					}}
+					>
+					<ListItem item={l} index={i} />
+				</li>
+			))}
 		</ul>
 	)
 }
